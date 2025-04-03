3490bf52f0b554c3d89bd4f7b2bcc0c9049748d0
diegomachado99 <160793872+diegomachado99@users.noreply.github.com>
2025-04-03 19:28:53 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded',function(){
 
-const botaoDeacessibilidade=document.gotElementByid('botao')
+    const botaoDeAcessibilidade=document.getElementById('botao-acessibilidade');
+    const opcoesDeAcessbilidade=document.getElementById('opcoes-acessibilidade');
+    
+    botaoDeAcessibilidade.addEventListener('click',function(){
+        botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+        opcoesDeAcessbilidade.classList.toggle('apresenta-lista');
+    
+    const botaoSelecionado=botaoDeAcessibilidade.getAttribute('aria-expanded')=== 'true';
 
-}
-)
+    botaoDeAcessibilidade.setAttributeO('aria-expanded', !botaoSelecionado)
+
+    })
+    
+    
+    const aumetafontebotao = document.getElementById('aumetar-fonte');
+    const diminuifontebotap = document.getElementById('diminuir-fonte');
+
+    const alternaContraste = document.getElementById('alterna-contraste')
+
+    
+    
+
+    
+    })
+
